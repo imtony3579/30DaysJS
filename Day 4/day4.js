aae497d61baa40f2276aad22456ff3df3b9ae0cd
imtony3579 <imtony3579@gmail.com>
2023-01-09 16:33:33 +0530

    day 4 completed

--- Day 4/day4.js
+++ Day 4/day4.js
@@ -3,7 +3,7 @@
 /* 
 1. Get user input using prompt(“Enter your age:”). If user is 18 or older , give feedback:'You are old enough to drive' but if not 18 give another feedback stating to wait for the number of years he needs to turn 18.
 */
-let age = prompt("Enter your Age", "Type your age here");
+// let age = prompt("Enter your Age", "Type your age here");
 // if (age >= 18) {
 //   alert("You are old enough to drive");
 // } else {
@@ -27,25 +27,25 @@ let age = prompt("Enter your Age", "Type your age here");
 using if else
 ternary operator.
 */
-let a = 4;
-let b = 5;
-if (a > b) {
-  console.log(`${a} is greater than ${b}`);
-} else if (b > a) {
-  console.log(`${b} is greater than ${a}`);
-}
-a > b
-  ? console.log(`${a} is greater than ${b}`)
-  : console.log(`${b} is greater than ${a}`);
+// let a = 4;
+// let b = 5;
+// if (a > b) {
+//   console.log(`${a} is greater than ${b}`);
+// } else if (b > a) {
+//   console.log(`${b} is greater than ${a}`);
+// }
+// a > b
+//   ? console.log(`${a} is greater than ${b}`)
+//   : console.log(`${b} is greater than ${a}`);
 
 /*
 4. Even numbers are divisible by 2 and the remainder is zero. How do you check, if a number is even or not using JavaScript?
 */
-if (age % 2 == 0) {
-  console.log(`${age} is an even number`);
-} else {
-  console.log(`${age} is an odd number`);
-}
+// if (age % 2 == 0) {
+//   console.log(`${age} is an even number`);
+// } else {
+//   console.log(`${age} is an odd number`);
+// }
 
 //--------Level 2-----------
 
@@ -57,23 +57,23 @@ if (age % 2 == 0) {
 50-59, D
 0-49, F
 */
-switch (true) {
-  case 80 < age && age < 100:
-    console.log("Your grade is A");
-    break;
-  case 70 < age && age < 89:
-    console.log("Your grade is B");
-    break;
-  case 60 < age && age < 69:
-    console.log("Your grade is C");
-    break;
-  case 50 < age && age < 59:
-    console.log("Your grade is D");
-    break;
-  case 0 < age && age < 49:
-    console.log("Your grade is F");
-    break;
-}
+// switch (true) {
+//   case 80 < age && age < 100:
+//     console.log("Your grade is A");
+//     break;
+//   case 70 < age && age < 89:
+//     console.log("Your grade is B");
+//     break;
+//   case 60 < age && age < 69:
+//     console.log("Your grade is C");
+//     break;
+//   case 50 < age && age < 59:
+//     console.log("Your grade is D");
+//     break;
+//   case 0 < age && age < 49:
+//     console.log("Your grade is F");
+//     break;
+// }
 
 /*
 2. Check if the season is Autumn, Winter, Spring or Summer. If the user input is :
@@ -82,17 +82,58 @@ December, January or February, the season is Winter.
 March, April or May, the season is Spring
 June, July or August, the season is Summer
 */
-let month = prompt("Enter month name").toLowerCase();
-if (month == "september" || month == "october" || month == "november") {
-  console.log("The season is Autumn.");
-} else if (month == "december" || month == "january" || month == "february") {
-  console.log("The season is Winter.");
-} else if (month == "march" || month == "april" || month == "may") {
-  console.log("The season is Spring.");
-} else if (month == "june" || month == "july" || month == "august") {
-  console.log("The season is Summer.");
-}
+// let month = prompt("Enter month name").toLowerCase();
+// if (month == "september" || month == "october" || month == "november") {
+//   console.log("The season is Autumn.");
+// } else if (month == "december" || month == "january" || month == "february") {
+//   console.log("The season is Winter.");
+// } else if (month == "march" || month == "april" || month == "may") {
+//   console.log("The season is Spring.");
+// } else if (month == "june" || month == "july" || month == "august") {
+//   console.log("The season is Summer.");
+// }
 
 /*
 Check if a day is weekend day or a working day. Your script will take day as an input.
 */
+let month = prompt("Enter a month:").toLowerCase();
+switch (month) {
+  case "january":
+    alert("January has 31 days.");
+    break;
+  case "february":
+    alert("February has 28 days.");
+    break;
+  case "march":
+    alert("March has 31 days.");
+    break;
+  case "april":
+    alert("April has 30 days.");
+    break;
+  case "may":
+    alert("May has 31 days.");
+    break;
+  case "june":
+    alert("June has 30 days.");
+    break;
+  case "july":
+    alert("July has 31 days.");
+    break;
+  case "august":
+    alert("August has 31 days.");
+    break;
+  case "september":
+    alert("September has 30 days.");
+    break;
+  case "october":
+    alert("October has 31 days.");
+    break;
+  case "november":
+    alert("November has 30 days.");
+    break;
+  case "december":
+    alert("December has 31 days.");
+    break;
+  default:
+    alert("Please type full month name");
+}
